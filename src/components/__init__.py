a0c3886eebdff037ec3810aaae6b8ddfaa061fa1
Adi <ripedown1@gmail.com>
2025-08-06 12:00:28 +0530

    history filter bug fix

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,4 +1,5 @@
 from src.components.session_memory import session_memory
 from src.components.execution_engine import execution_engine
 from src.components.output_processor import output_processor
-from src.components import input_router
+from src.components.input_router import input_router
+from src.components.history_filter import history_filter
